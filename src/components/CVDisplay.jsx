1d90649d7b4adc72c355cf2e5df9ec7fe0c442d2
Eriwe-li <11926638+zakingslayerv22@users.noreply.github.com>
2025-10-23 09:43:47 -0400

    Style CVDisplay
    In CVDisplay.jsx,
    change the JSX
    fragments wrapping
    the whole display
    elements into
    a div element
    with a class
    of container.
    
    Move the
    basic-information
    section the
    age and sex above
    the add email and
    number. Add the
    address.
    Remove its h3
    containing its section
    title.
    
    Create a CVDisplay
    css file and
    style the .container
    and
    .basic-information

--- src/components/CVDisplay.jsx
+++ src/components/CVDisplay.jsx
@@ -1,17 +1,17 @@
 export default function CVDisplay({ CVDataArray, handleEdit }) {
   return (
-    <>
-      <section className="basic-info">
-        <h3>{CVDataArray[0].sectionTitle}</h3>
-        <div>
+    <div className="container">
+      <section className="basic-information">
+        <h2>
           {CVDataArray[0].firstName} {CVDataArray[0].lastName}
+        </h2>
+        <div>
+          {CVDataArray[0].age} | {CVDataArray[0].sex}
         </div>
         <div>
           {CVDataArray[0].email} | {CVDataArray[0].phoneNumber}
         </div>
-        <div>
-          {CVDataArray[0].age} | {CVDataArray[0].sex}
-        </div>
+        <div>{CVDataArray[0].address}</div>
       </section>
       <hr />
       <section className="educational-qualifications">
@@ -36,6 +36,6 @@ export default function CVDisplay({ CVDataArray, handleEdit }) {
       </section>
       <br />
       <button onClick={handleEdit}>Edit</button>
-    </>
+    </div>
   );
 }
