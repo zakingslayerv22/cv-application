30e68ec3f64a5550f4a1c4abeec280f971c98fa6
Eriwe-li <11926638+zakingslayerv22@users.noreply.github.com>
2025-10-26 13:07:15 -0400

    Change CVDisplay structure
    In CVDisplay.jsx, wrap all
    the JSX except the button in
    a div. This is so that with
    the flex display and a
    min-height of 100vh for the
    .container, this div can
    be given a flex-grow of 1.
    This will make it to
    expand and push the
    button to the bottom
    of the page (in case this
    div's content is small)
    
    Add a class to the button
    as it will have
    peculiar styles.

--- src/components/CVDisplay.jsx
+++ src/components/CVDisplay.jsx
@@ -5,66 +5,70 @@ import locationIcon from "../assets/images/icons/map-marker-radius.svg";
 export default function CVDisplay({ CVDataArray, handleEdit }) {
   return (
     <div className="container">
-      <section className="basic-information">
-        <h2>
-          {CVDataArray[0].firstName} {CVDataArray[0].lastName}
-        </h2>
-        <div className="email-and-phone">
-          <div className="details-container">
-            <img src={emailIcon} className="icon" alt="email-icon" />
-            <div>{CVDataArray[0].email}</div>
+      <div>
+        <section className="basic-information">
+          <h2>
+            {CVDataArray[0].firstName} {CVDataArray[0].lastName}
+          </h2>
+          <div className="email-and-phone">
+            <div className="details-container">
+              <img src={emailIcon} className="icon" alt="email-icon" />
+              <div>{CVDataArray[0].email}</div>
+            </div>
+            <div className="details-container">
+              <img src={phoneIcon} className="icon" alt="phone-icon" />
+              <div>{CVDataArray[0].phoneNumber}</div>
+            </div>
           </div>
           <div className="details-container">
-            <img src={phoneIcon} className="icon" alt="phone-icon" />
-            <div>{CVDataArray[0].phoneNumber}</div>
+            <img src={locationIcon} className="icon" alt="location-icon" />
+            <div>{CVDataArray[0].address}</div>
           </div>
-        </div>
-        <div className="details-container">
-          <img src={locationIcon} className="icon" alt="location-icon" />
-          <div>{CVDataArray[0].address}</div>
-        </div>
-      </section>
-      <section className="educational-qualifications">
-        <h3 className="title-of-section">{CVDataArray[1].sectionTitle}</h3>
-        <div className="education-details">
-          <div>
-            <div>{CVDataArray[1].location} </div>
-            <div> {CVDataArray[1].dateOfGraduation}</div>
-          </div>
-          <div>
+        </section>
+        <section className="educational-qualifications">
+          <h3 className="title-of-section">{CVDataArray[1].sectionTitle}</h3>
+          <div className="education-details">
             <div>
-              <strong>{CVDataArray[1].schoolName}</strong>
+              <div>{CVDataArray[1].location} </div>
+              <div> {CVDataArray[1].dateOfGraduation}</div>
             </div>
             <div>
-              <strong>{CVDataArray[1].course}</strong>
-            </div>
+              <div>
+                <strong>{CVDataArray[1].schoolName}</strong>
+              </div>
+              <div>
+                <strong>{CVDataArray[1].course}</strong>
+              </div>
 
-            <div>{CVDataArray[1].certificateObtained}</div>
-          </div>
-        </div>
-      </section>
-      <section className="work-experience">
-        <h3 className="title-of-section">{CVDataArray[2].sectionTitle}</h3>
-        <div className="work-details">
-          <div>
-            <div> {CVDataArray[2].location}</div>
-            <div>
-              {CVDataArray[2].from} - {CVDataArray[2].to}
+              <div>{CVDataArray[1].certificateObtained}</div>
             </div>
           </div>
-          <div>
+        </section>
+        <section className="work-experience">
+          <h3 className="title-of-section">{CVDataArray[2].sectionTitle}</h3>
+          <div className="work-details">
             <div>
-              <strong>{CVDataArray[2].companyName}</strong>
+              <div> {CVDataArray[2].location}</div>
+              <div>
+                {CVDataArray[2].from} - {CVDataArray[2].to}
+              </div>
             </div>
             <div>
-              <strong>{CVDataArray[2].positionTitle}</strong>
+              <div>
+                <strong>{CVDataArray[2].companyName}</strong>
+              </div>
+              <div>
+                <strong>{CVDataArray[2].positionTitle}</strong>
+              </div>
+              <div>{CVDataArray[2].responsibilities}</div>
             </div>
-            <div>{CVDataArray[2].responsibilities}</div>
           </div>
-        </div>
-      </section>
-      <br />
-      <button onClick={handleEdit}>Edit</button>
+        </section>
+        <br />
+      </div>
+      <button onClick={handleEdit} className="edit-button">
+        Edit
+      </button>
     </div>
   );
 }
